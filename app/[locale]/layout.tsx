fcecc46a66cd4dd18f9d92a112802c0f5998e70e
pony <13500815917@163.com>
2024-06-18 15:18:23 +0800

    fix @tanstack/react-query

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -67,6 +67,7 @@ export default function RootLayout({
               <div className="relative flex min-h-screen flex-col">
                 <SiteHeader />
                 {/* <main className="flex-1 relative">{children}</main> */}
+                <p>this赛积分</p>
                 <Footer />
               </div>
               <NetworkStatus />
